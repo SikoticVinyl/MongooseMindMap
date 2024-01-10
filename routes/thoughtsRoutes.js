d3e6e8d21ea8fb25c834a9266800142e0e5b02fb
Dana <68112012+SikoticVinyl@users.noreply.github.com>
2024-01-10 00:57:49 -0700

    Post to create a new thought added.

--- routes/thoughtsRoutes.js
+++ routes/thoughtsRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Thought = require('../models/Thought');
 const Reaction = require('../models/Reaction');
+const User = require('../models/User'); 
 
 // GET all thoughts
 router.get('/', async (req, res) => {
@@ -27,6 +28,25 @@ router.get('/:id', async (req, res) => {
     res.json(thought);
   } catch (err) {
     res.status(500).json(err);
+  }
+});
+
+// POST to create a new thought
+router.post('/', async (req, res) => {
+  try {
+    const thoughtData = req.body;
+    const newThought = await Thought.create(thoughtData);
+
+    // Push the created thought's _id to the associated user's thoughts array field
+    const userId = newThought.userId;
+    if (userId) {
+      await User.findByIdAndUpdate(userId, { $push: { thoughts: newThought._id } });
+    }
+
+    res.status(201).json(newThought);
+  } catch (err) {
+    console.error('Error creating thought:', err.message); // Log the error message
+    res.status(400).json(err);
   }
 });
 
