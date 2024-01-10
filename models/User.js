ead8d249c8cda000fbcc8cbfa6d5bb7cad91c3d4
Dana <68112012+SikoticVinyl@users.noreply.github.com>
2024-01-09 20:31:30 -0700

    user.js built out.

--- models/User.js
+++ models/User.js
@@ -12,5 +12,25 @@ const userSchema = new mongoose.Schema({
         required: true,
         unique: true,
         match: [/.+@.+\..+/, 'Please enter a valid email address']
-      },
-    });
+    },
+    thoughts: [
+        {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'Thought'
+        }
+    ],
+    friends: [
+        {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'User'
+        }
+      ]
+});
+
+userSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+  });
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
