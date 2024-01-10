0d7e51a2ad0523f8c7bb77e679963ef9f06b555d
Dana <68112012+SikoticVinyl@users.noreply.github.com>
2024-01-09 20:38:43 -0700

    thoughtSchema started and built through

--- models/Thought.js
+++ models/Thought.js
@@ -1,1 +1,35 @@
 const mongoose = require('mongoose');
+
+const thoughtSchema = new mongoose.Schema({
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: function (createdAtVal) {
+        const options = {
+          month: '2-digit',
+          day: '2-digit',
+          year: '2-digit',
+          hour: '2-digit',
+          minute: '2-digit',
+          hour12: true
+        };
+        return createdAtVal.toLocaleString('en-US', options);
+      }
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    reactions: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Reaction'
+      }
+    ]
+  });
