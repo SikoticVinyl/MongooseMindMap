bd866f95e07930521de07868681cab6ba79d6135
Dana <68112012+SikoticVinyl@users.noreply.github.com>
2024-01-09 20:27:11 -0700

    began to build userSchema for db.

--- models/User.js
+++ models/User.js
@@ -1,1 +1,16 @@
 const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/, 'Please enter a valid email address']
+      },
+    });
