a982e0b1d77325af8cf7c353a4c401850f091db7
Dana <68112012+SikoticVinyl@users.noreply.github.com>
2024-01-09 22:39:04 -0700

    added get user by id route.

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -12,4 +12,18 @@ router.get('/', async (req, res) => {
   }
 });
 
+// GET a single user by _id and populate thought and friend data
+router.get('/:id', async (req, res) => {
+    try {
+      const userId = req.params.id;
+      const user = await User.findById(userId).populate('thoughts').populate('friends');
+      if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
 module.exports = router;
